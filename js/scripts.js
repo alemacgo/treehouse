7cc5dbc3f184787e2dfb657b9d4a592f6ba41151
Alejandro Machado <alemacgo@gmail.com>
2016-01-17 22:17:27 +0100

    Fix bug in mobile Safari that made project links unclickable

--- js/scripts.js
+++ js/scripts.js
@@ -1150,14 +1150,19 @@ var mr_cookies = {
   }
 };
 
-//$(function () {    
-//    $('a').on('click touchend', function() { 
-//        var link = $(this).attr('href');   
-//        window.open(link,'_blank'); // opens in new window as requested 
-//
-//        return false; // prevent anchor click    
-//    });    
-//});
+$(function () {    
+    IS_IPAD = navigator.userAgent.match(/iPad/i) != null;
+    IS_IPHONE = (navigator.userAgent.match(/iPhone/i) != null) || (navigator.userAgent.match(/iPod/i) != null);
+
+    if (IS_IPAD || IS_IPHONE) {
+        $('a').on('click touchend', function() { 
+            var link = $(this).attr('href');   
+            window.open(link); // opens in new window as requested 
+
+            return false; // prevent anchor click    
+        });    
+    }
+});
 /*\
 |*|  END COOKIE LIBRARY
 \*/
